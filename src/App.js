78ee60810cde9972d8862c207b8de3756adbd808
lexie70 <alexandracampos658@gmail.com>
2023-05-13 01:12:45 +0000

    falta mostrar resultado del filtro

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState} from 'react';
+import React from 'react';
 
 function App() {
+  const[post,setPost]=useState([])
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredPosts, setFilteredPosts] = useState([]);
+
+
+useEffect(()=>{
+  fetch("https://dummyjson.com/posts")
+  .then(res=>res.json())
+  .then(data=>{setPost(data.posts);})
+},[])
+
+
+
+const handleFilter = () => {
+  const toFilter = post.filter((item) =>
+    item.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  setFilteredPosts(toFilter);
+};
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={()=> console.log(filteredPosts)}> Boton </button>
+        <Filter searchTerm={searchTerm}
+        setSearchTerm={setSearchTerm}
+        handleFilter={handleFilter}/>
+      <ul>
+        <li onClick={()=>console.log(post)}>casa</li>
+        {filteredPosts.map((item, index) => {
+          return <li key={index}><h1>{item.title}</h1>{item.body}</li>;
+        })}
+      </ul>
     </div>
   );
 }
 
 export default App;
+
+const Filter= ({ searchTerm, setSearchTerm, handleFilter })=>{
+  return <>
+<input type="text" onChange={(e)=>{setSearchTerm(e.target.value);console.log(searchTerm);}}/>
+<button onClick={()=>{handleFilter()}}>Filter</button>
+  </>
+}
+ 
+  
+
